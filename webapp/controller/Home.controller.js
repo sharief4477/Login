c3153d3e9d9d52ed2fd5b9f30515453aa7c3f93f
Sharief Ahamed <shariefahamed@sbpcorp.in>
2025-05-12 02:45:44 +0000

    loginxml

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,10 +1,33 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    'sap/m/MessageToast'
+], (Controller,MessageToast) => {
     "use strict";
 
     return Controller.extend("cpapp.login.controller.Home", {
         onInit() {
+        },
+        onLogin:function(){
+            let sId = this.getView().byId("userId").getValue();
+            let sPassword = this.getView().byId("password").getValue();
+
+            if(!sId && !sPassword){
+                MessageToast.show("Please enter both User ID and Password.");
+                return;
+            }
+
+            const oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
+
+            oCrossAppNavigator.toExternal({
+                target: {
+                    semanticObject: "getStockInfo",    // Replace with your semantic object
+                    action: "display"              // Replace with your action
+                },
+                params: {
+                    "userId": sId
+                }
+            });
+
         }
     });
 });
